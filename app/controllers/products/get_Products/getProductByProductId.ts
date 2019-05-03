b8e9e7f51da0d11f5861a046e4494dff6f250e7d
Shubham Gupta <sgshubham04@gmail.com>
2019-05-03 18:03:25 +0530

    New

--- app/controllers/products/get_Products/getProductByProductId.ts
+++ app/controllers/products/get_Products/getProductByProductId.ts
@@ -6,11 +6,13 @@ const getProductsByProductId=(req:Request,res:Response)=>{
 let prod_id:number = req.params.prod_id
 
 ProductListModel.find({_id:prod_id})
-.then(result=>{
-    res.status(200).json({success:"true",message:"Product Fetched",product_details:result})
-})
-.catch(err=>{
-    res.status(404).json({success:"false",message:"Something went wrong",error_message:err})
+.populate('categ_id')
+.populate('color_id')
+.exec((err,product)=>{
+    if(err)
+    res.status(404).json({success:"false",error_message:err})
+    else
+    res.status(200).json({success:product.length!==0?"true":"false",message:product.length!==0?"Product by product id":"No Data is available",product_details:product.length!==0?product:"No details are available"})
 })
 
 }
