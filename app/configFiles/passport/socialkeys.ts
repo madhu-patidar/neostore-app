a7ec9630951a662f4979465d67799ac9ef29bb91
Shubham Gupta <sgshubham04@gmail.com>
2019-05-31 09:42:11 +0530

    Remove

--- app/configFiles/passport/socialkeys.ts
+++ app/configFiles/passport/socialkeys.ts
@@ -9,9 +9,6 @@ export const keys={
         clientSecret:'ff195ab7c1c8d82f44f82eb05688e3d5',
         callbackURL:'http://localhost:5000/auth/facebook/redirect'
     },
-    database:{
-        URL:'mongodb://localhost:27017/socialLogin'
-    },
     session:{
     	cookieKey:'NeoSOFTTechnologies'
     }
